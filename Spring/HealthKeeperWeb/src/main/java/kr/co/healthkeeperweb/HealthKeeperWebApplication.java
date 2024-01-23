af488f1785177da725e07f78d6de1923a93c2d95
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-01-23 11:37:14 +0900

    project build

---
+++ Spring/HealthKeeperWeb/src/main/java/kr/co/healthkeeperweb/HealthKeeperWebApplication.java
@@ -0,0 +1,13 @@
+package kr.co.healthkeeperweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthKeeperWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthKeeperWebApplication.class, args);
+    }
+
+}
