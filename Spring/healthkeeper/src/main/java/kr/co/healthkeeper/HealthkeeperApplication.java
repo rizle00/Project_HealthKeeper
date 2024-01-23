9d18b3c50c4835c055b1404fe3c6d47f38a9d9ca
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-01-23 15:00:52 +0900

    11

--- Spring/healthkeeper/src/main/java/kr/co/healthkeeper/HealthkeeperApplication.java
+++ Spring/healthkeeper/src/main/java/kr/co/healthkeeper/HealthkeeperApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HealthkeeperApplication {
+public class    HealthkeeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthkeeperApplication.class, args);
