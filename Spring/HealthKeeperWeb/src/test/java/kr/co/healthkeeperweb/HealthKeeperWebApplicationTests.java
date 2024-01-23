b3a382fc451031223494aef99cfa2a9b47cb269b
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-01-23 11:37:14 +0900

    project build

---
+++ Spring/HealthKeeperWeb/src/test/java/kr/co/healthkeeperweb/HealthKeeperWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.healthkeeperweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthKeeperWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
