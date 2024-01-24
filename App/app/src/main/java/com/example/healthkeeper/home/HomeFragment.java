82e682b2cebaa203bf33ffd3f6972755da90222e
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2024-01-24 14:46:18 +0900

    and

---
+++ App/app/src/main/java/com/example/healthkeeper/home/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.healthkeeper.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.healthkeeper.R;
+import com.example.healthkeeper.databinding.ActivityMain2Binding;
+
+
+public class HomeFragment extends Fragment {
+    ActivityMain2Binding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
