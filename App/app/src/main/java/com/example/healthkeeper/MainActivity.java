9ae1abf09b7da58a358b57b1fdc147c049e1a675
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2024-01-24 14:16:39 +0900

    android

--- App/app/src/main/java/com/example/healthkeeper/MainActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.healthkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 
-import com.example.healthkeeperapp.R;
+import com.example.healthkeeper.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Log.d(TAG, "onCreate");
+
+
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+            finish(); // 선택 사항: 새로 시작한 활동 이후 현재 활동을 종료합니다.
+        }, 2000);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
