597ccb56db35f20ea2690e8ae166369f46d7238c
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2024-01-25 11:33:29 +0900

    and
    login

--- App/app/src/main/java/com/example/healthkeeper/login/LoginActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/login/LoginActivity.java
@@ -8,6 +8,14 @@ import com.example.healthkeeper.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /* 화면 전환 -> intent
+    아이디 비밀번호 -> finish() xx
+    회원가입 -> finish()*/
+
+    /*
+    https://developer.android.com/training/transitions/start-activity?hl=ko
+    intent animation
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
