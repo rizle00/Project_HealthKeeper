17ae167ca7363b2dde40de10c3a3665cfd698f07
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-01-30 15:57:36 +0900

    1.30 블루투스 진행중

---
+++ App/app/src/main/java/com/example/healthkeeper/bt/BTtestActivity.java
@@ -0,0 +1,258 @@
+package com.example.healthkeeper.bt;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import com.example.healthkeeper.R;
+import com.example.healthkeeper.databinding.ActivityBttestBinding;
+import com.example.healthkeeper.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public class BTtestActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
+    private static final int REQUEST_ENABLE_BT = 10; // 블루투스 활성화상태
+    private BluetoothAdapter bluetoothAdapter; // 블루투스 어댑터
+    private Set<BluetoothDevice> devices; // 블루투스 디바이스 데이터 셋
+    private BluetoothDevice bluetoothDevice; // 블루투스 디바이스
+    private BluetoothSocket bluetoothSocket = null; // 블루투스 소켓
+    private OutputStream outputStream = null; // 블루투스에 데이터를 출력하기 위한 출력 스트림
+    private InputStream inputStream = null; // 블루투스에 데이터를 입력하기 위한 입력 스트림
+    private Thread workerThread = null; // 문자열 수신에 사용되는 쓰레드
+    private byte[] readBuffer; // 수신 된 문자열을 저장하기 위한 버퍼
+    private int readBufferPosition; // 버퍼 내 문자 저장 위치
+    int pariedDeviceCount;
+    ActivityBttestBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityBttestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SetBluetooth();
+        binding.
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void SetBluetooth() {
+        // 블루투스 활성화하기
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 블루투스 어댑터를 디폴트 어댑터로 설정
+        if (bluetoothAdapter == null) { // 디바이스가 블루투스를 지원하지 않을 때
+            Toast.makeText(getApplicationContext(), "블루투스 미지원 기기입니다.", Toast.LENGTH_LONG).show();
+            // 여기에 처리 할 코드를 작성하세요.
+        } else { // 디바이스가 블루투스를 지원 할 때
+
+            if (bluetoothAdapter.isEnabled()) { // 블루투스가 활성화 상태 (기기에 블루투스가 켜져있음)
+
+                selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+            } else { // 블루투스가 비 활성화 상태 (기기에 블루투스가 꺼져있음)
+
+                // 블루투스를 활성화 하기 위한 다이얼로그 출력
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // 선택한 값이 onActivityResult 함수에서 콜백된다.
+
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+
+            }
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void selectBluetoothDevice() {
+
+        // 이미 페어링 되어있는 블루투스 기기를 찾습니다.
+        devices = bluetoothAdapter.getBondedDevices();
+
+        // 페어링 된 디바이스의 크기를 저장
+        pariedDeviceCount = devices.size();
+
+        // 페어링 되어있는 장치가 없는 경우
+        if(pariedDeviceCount == 0) {
+            // 페어링을 하기위한 함수 호출
+            Toast.makeText(getApplicationContext(), "먼저 Bluetooth 설정에 들어가 페어링 해주세요", Toast.LENGTH_SHORT).show();
+        }
+
+        // 페어링 되어있는 장치가 있는 경우
+        else {
+            // 디바이스를 선택하기 위한 다이얼로그 생성
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("페어링 되어있는 블루투스 디바이스 목록");
+            // 페어링 된 각각의 디바이스의 이름과 주소를 저장
+            List<String> list = new ArrayList<>();
+
+            // 모든 디바이스의 이름을 리스트에 추가
+            for(BluetoothDevice bluetoothDevice : devices) {
+                list.add(bluetoothDevice.getName());
+            }
+
+            list.add("취소");
+
+
+
+            // List를 CharSequence 배열로 변경
+            final CharSequence[] charSequences = list.toArray(new CharSequence[list.size()]);
+
+            list.toArray(new CharSequence[list.size()]);
+
+
+
+            // 해당 아이템을 눌렀을 때 호출 되는 이벤트 리스너
+
+            builder.setItems(charSequences, new DialogInterface.OnClickListener() {
+
+                @Override
+
+                public void onClick(DialogInterface dialog, int which) {
+
+                    // 해당 디바이스와 연결하는 함수 호출
+                    connectDevice(charSequences[which].toString());
+
+                }
+
+            });
+
+
+
+            // 뒤로가기 버튼 누를 때 창이 안닫히도록 설정
+
+            builder.setCancelable(false);
+
+            // 다이얼로그 생성
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    public void connectDevice(String deviceName) {
+        // 페어링 된 디바이스들을 모두 탐색
+        for(BluetoothDevice tempDevice : devices) {
+            // 사용자가 선택한 이름과 같은 디바이스로 설정하고 반복문 종료
+            if(deviceName.equals(tempDevice.getName())) {
+                bluetoothDevice = tempDevice;
+                break;
+            }
+        }
+        Toast.makeText(this, bluetoothDevice +"연결 완료", Toast.LENGTH_SHORT).show();
+        // UUID 생성
+
+        UUID uuid = java.util.UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        // Rfcomm 채널을 통해 블루투스 디바이스와 통신하는 소켓 생성
+        try {
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+            bluetoothSocket.connect();
+            outputStream = bluetoothSocket.getOutputStream();
+            inputStream = bluetoothSocket.getInputStream();
+
+            // 데이터 수신 함수 호출
+            receiveData();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void receiveData() {
+
+        final Handler handler = new Handler();
+        // 데이터를 수신하기 위한 버퍼를 생성
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+
+        // 데이터를 수신하기 위한 쓰레드 생성
+        workerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while(true) {
+
+                    if (Thread.currentThread().isInterrupted()) {
+                        break;
+                    }
+
+                    try {
+                        // 데이터를 수신했는지 확인합니다.
+                        int byteAvailable = inputStream.available();
+                        // 데이터가 수신 된 경우
+                        if(byteAvailable > 0) {
+                            // 입력 스트림에서 바이트 단위로 읽어 옵니다.
+
+                            byte[] bytes = new byte[byteAvailable];
+
+                            inputStream.read(bytes);
+
+                            // 입력 스트림 바이트를 한 바이트씩 읽어 옵니다.
+                            for(int i = 0; i < byteAvailable; i++) {
+                                byte tempByte = bytes[i];
+                                // 개행문자를 기준으로 받음(한줄)
+                                if(tempByte == '\n') {
+                                    // readBuffer 배열을 encodedBytes로 복사
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    // 인코딩 된 바이트 배열을 문자열로 변환
+                                    final String text = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable() {
+
+                                        @Override
+                                        public void run() {
+                                            // 아두이노에서 받은 출력값
+                                            Log.d(TAG, "run: text ="+ text);
+
+                                        }
+                                    });
+                                } // 개행 문자가 아닐 경우
+                                else {
+                                    readBuffer[readBufferPosition++] = tempByte;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        // 1초마다 받아옴
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        workerThread.start();
+    }
+
+}
