c043f80d095156aa255c75dbd16411df9ce902f7
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2024-01-31 12:56:41 +0900

    asd
    asd

--- App/app/src/main/java/com/example/healthkeeper/member/MemberJoinActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/member/MemberJoinActivity.java
@@ -1,12 +1,12 @@
-package com.example.healthkeeper.login;
+package com.example.healthkeeper.member;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -15,6 +15,7 @@ import com.example.healthkeeper.R;
 import com.example.healthkeeper.databinding.ActivityMemberJoinBinding;
 
 public class MemberJoinActivity extends AppCompatActivity {
+    MemberVO vo;
     ActivityMemberJoinBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +23,39 @@ public class MemberJoinActivity extends AppCompatActivity {
         binding = ActivityMemberJoinBinding.inflate(getLayoutInflater());
 
 
+        /* 혈액형 */
+        Spinner bloodTypeSpn = (Spinner)binding.spnBloodType;
+        ArrayAdapter bloodAdapter = ArrayAdapter.createFromResource(this,R.array.bloodType, android.R.layout.simple_spinner_item);
+        bloodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        bloodTypeSpn.setAdapter(bloodAdapter);
+
         /* 아이디 글자수 확인 */
+       IdLength();
+
+        binding.btnJoin.setOnClickListener(v -> {
+            joinClick();
+        });
+
+
+        setContentView(binding.getRoot());
+    }
+
+
+
+    public boolean idCheck(){
+        binding.btnIdCheck.setOnClickListener(v -> {
+
+        });
+        /* 아이디 중복체크 완료되면 체크표시 */
+        binding.edtUserId.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.img_check,0);
+        return false;
+    }
+
+    public void joinClick(){
+            bloodCheck();
+    }
+
+    public void IdLength(){
         binding.edtUserId.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -47,23 +80,21 @@ public class MemberJoinActivity extends AppCompatActivity {
 
             }
         });
-
-        Spinner bloodTypeSpn = (Spinner)binding.spnBloodType;
-        ArrayAdapter bloodAdapter = ArrayAdapter.createFromResource(this,R.array.bloodType, android.R.layout.simple_spinner_item);
-        bloodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        bloodTypeSpn.setAdapter(bloodAdapter);
-
-
-
-        setContentView(binding.getRoot());
     }
 
-    public boolean id_check(){
-        binding.btnIdCheck.setOnClickListener(v -> {
+    public void bloodCheck() {
+        Spinner bloodType = (Spinner) binding.spnBloodType;
+        String blood = bloodType.getSelectedItem().toString();
+        if (blood.equals("혈액형")) {
+            Log.d("실패", blood);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("").setMessage("혈액형을 확인해주세요");
+            builder.show();
 
-        });
-        /* 아이디 중복체크 완료되면 체크표시 */
-        binding.edtUserId.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.img_check,0);
-        return false;
+        }
+        else{
+            Log.d("성공", blood);
+            vo.setBlood(blood);
+        }
     }
 }
