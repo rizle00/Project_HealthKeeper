43a0122bfaadd040ccafb90f6e1495c77c675b26
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2024-02-01 18:06:45 +0900

    1

--- App/app/src/main/java/com/example/healthkeeper/main/CommunityActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/main/CommunityActivity.java
@@ -3,12 +3,15 @@ package com.example.healthkeeper.main;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.healthkeeper.R;
+import com.example.healthkeeper.databinding.ActivityCommunityBinding;
+import com.example.healthkeeper.databinding.ActivityMainBinding;
 
 public class CommunityActivity extends AppCompatActivity {
-
+ActivityCommunityBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_community);
+        binding = ActivityCommunityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
