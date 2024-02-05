8ca60f3da60686a486b33c178fe5d8da8cd0eb2c
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2024-02-05 17:59:59 +0900

    Revert "and"
    This reverts commit 6889eb2fe89b86462b86db1c721cfa19595e8f1b, reversing
    changes made to 395bd6848aec52524fa79a717d00ba0cac01e277.

--- App/app/src/main/java/com/example/healthkeeper/home/HomeFragment.java
+++ App/app/src/main/java/com/example/healthkeeper/home/HomeFragment.java
@@ -1,16 +1,21 @@
-package com.example.healthkeeper.main;
+package com.example.healthkeeper.home;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.healthkeeper.R;
+import com.example.healthkeeper.databinding.ActivityMainBinding;
+import com.example.healthkeeper.databinding.ActivityMainBinding;
 import com.example.healthkeeper.databinding.FragmentHomeBinding;
+import com.example.healthkeeper.main.ConditionActivity;
+import com.example.healthkeeper.main.MainActivity;
 import com.example.healthkeeper.member.LoginActivity;
 
 
@@ -39,7 +44,7 @@ public class HomeFragment extends Fragment {
 
     }
     private void changeConditionFragment() {
-        Log.d("HomeFragment", "Changing to ConditionActivity");
+
         Intent intent = new Intent(getActivity(), ConditionActivity.class);
         startActivity(intent);
     }
--- App/app/src/main/java/com/example/healthkeeper/main/CommunityActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/main/CommunityActivity.java
@@ -3,15 +3,12 @@ package com.example.healthkeeper.main;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.healthkeeper.R;
-import com.example.healthkeeper.databinding.ActivityCommunityBinding;
-import com.example.healthkeeper.databinding.ActivityMainBinding;
 
 public class CommunityActivity extends AppCompatActivity {
-ActivityCommunityBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityCommunityBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_community);
     }
 }
