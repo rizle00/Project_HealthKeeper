dae0e8b792c90a21b93092a885fa2ee6624b5bc2
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2024-02-14 17:52:18 +0900

    0214

--- App/app/src/main/java/com/example/healthkeeper/main/MainActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/main/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.healthkeeper.main;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.healthkeeper.R;
 import com.example.healthkeeper.databinding.ActivityMainBinding;
@@ -13,7 +18,7 @@ import com.example.healthkeeper.setting.MyMenuFragment;
 public class MainActivity extends AppCompatActivity {
     /*다른 액티비티에서 MainActivity 종료를 위한 선언*/
     public static MainActivity _mainActivity;
-
+    private AlertDialog alertDialog;
     ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 changeFragment(new HomeFragment());
                 return true;
             } else if (itemId == R.id.nav_119) {
-
+                showEmergencyDialog();
                 return true;
             } else if (itemId == R.id.nav_commu) {
 
@@ -55,5 +60,46 @@ public class MainActivity extends AppCompatActivity {
     public void changeFragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.container , fragment).commit();
     }
+
+
+    //긴급전화 다이얼로그처리
+    private void showEmergencyDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_emergency, null);
+        builder.setView(dialogView);
+
+        TextView dialogTitle = dialogView.findViewById(R.id.dialogTitle);
+        TextView dialogMessage = dialogView.findViewById(R.id.dialogMessage);
+        Button confirmButton = dialogView.findViewById(R.id.confirmButton);
+        Button cancelButton = dialogView.findViewById(R.id.cancelButton);
+
+
+        dialogTitle.setText("긴급 전화");
+        dialogMessage.setText("긴급 전화 연결됩니다.\n 연결하시겠습니까?");
+
+        confirmButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent= new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:긴급전화번호"));
+                startActivity(callIntent);
+
+                 alertDialog.dismiss();
+            }
+        }));
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog = builder.create();
+
+        alertDialog.show();
+    }
+
+
 
 }
