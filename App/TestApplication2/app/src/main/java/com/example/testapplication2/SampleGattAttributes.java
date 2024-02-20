a88c0348f4005998def2295008de298ade9f0858
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-02-20 19:51:41 +0900

    2.20 bt 진행중

--- App/TestApplication2/app/src/main/java/com/example/testapplication2/SampleGattAttributes.java
+++ App/TestApplication2/app/src/main/java/com/example/testapplication2/SampleGattAttributes.java
@@ -11,6 +11,7 @@ public class SampleGattAttributes {
         // Sample Services.
         attributes.put("0000ffe0-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+//        attributes.put("0000180f-0000-1000-8000-00805f9b34fb", "Battery Service");
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
