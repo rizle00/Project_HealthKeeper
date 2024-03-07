024b7f84aae72ec49a68139ec347a6e476859692
rizle00 <144658576+rizle00@users.noreply.github.com>
2024-03-07 17:14:24 +0900

    ㅁ

--- App/app/src/main/java/com/example/healthkeeper/main/MainActivity.java
+++ App/app/src/main/java/com/example/healthkeeper/main/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.healthkeeper.main;
 
 import static android.app.PendingIntent.getActivity;
 
+import android.app.NotificationManager;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.service.notification.StatusBarNotification;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,13 +27,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.healthkeeper.R;
+import com.example.healthkeeper.bluetooth.BluetoothConnector;
+import com.example.healthkeeper.bluetooth.BluetoothService;
+import com.example.healthkeeper.bluetooth.BtActivity;
 import com.example.healthkeeper.databinding.ActivityMainBinding;
 import com.example.healthkeeper.setting.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    //블루투스 관련 ---------------
+    private static final int INTENT_REQUEST_BLUETOOTH_ENABLE = 0x0701;
+    private BluetoothService bluetoothService;
+    private boolean sBound, active;// gatt 서비스, bluetooth 서비스 연결 체크
+    private final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothConnector btConnector;
+    //------------------------------------
     private AlertDialog alertDialog;
-    ActivityMainBinding binding;
-
+    private ActivityMainBinding binding;
 
     private boolean doubleBackToExitPressedOnce = false;
 
@@ -38,202 +52,159 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        active = isForegroundServiceRunning(this, 2000);
 
-
-        // 툴바 추가
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        int customTextColor = getResources().getColor(R.color.white);//툴바의 타이틀 생상변경시 사용
+        int customTextColor = getResources().getColor(R.color.white);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//뒤로가기 버튼만들기
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//툴바에 뒤로가기 아이콘 생성
 
         getSupportActionBar().setTitle(getColoredSpanned("홈", customTextColor));
-        changeFragment(new HomeFragment());
 
+        changeFragment(new HomeFragment());//화면에 처음 보여질 fragment설정!!
 
+        bottomNavSetting();
+    }
+
+    private void bottomNavSetting() {
         binding.bottomNav.setOnNavigationItemSelectedListener(item -> {
-            // btm_nav에 메뉴를 눌렀을때 툴바에 표시될 타이틀,메뉴와 색상지정!
             int itemId = item.getItemId();
-            int customTextColor = getResources().getColor(R.color.white);
+            int customTextColorNav = getResources().getColor(R.color.white);
 
             if (itemId == R.id.nav_home) {
                 changeFragment(new HomeFragment());
-                getSupportActionBar().setTitle(getColoredSpanned("홈", customTextColor));//툴바에 표시될 타이틀과 색상지정!
-                getSupportActionBar().setTitle(getColoredSpanned("홈", customTextColor));
+                getSupportActionBar().setTitle(getColoredSpanned("홈", customTextColorNav));
                 setToolbarMenu(R.menu.toolbar_home);
-
                 return true;
             } else if (itemId == R.id.nav_119) {
-                getSupportActionBar().setTitle(getColoredSpanned("긴급연락", customTextColor));
                 showEmergencyDialog();
-
-                getSupportActionBar().setTitle(getColoredSpanned("긴급연락", customTextColor));
                 return true;
             } else if (itemId == R.id.nav_commu) {
-                //changeScheduleFragment(new ());
-                getSupportActionBar().setTitle(getColoredSpanned("CUMUNITY", customTextColor));
-                setToolbarMenu(R.menu.toolbar_cummunity);
                 Intent intent = new Intent(this, CommunityActivity.class);
                 startActivity(intent);
                 finish();
-
-
                 return true;
             } else if (itemId == R.id.nav_schedule) {
-                changeScheduleFragment(new ScheduleFragment());
                 changeFragment(new ScheduleFragment());
-
-                getSupportActionBar().setTitle(getColoredSpanned("일정관리", customTextColor));
+                getSupportActionBar().setTitle(getColoredSpanned("일정관리", customTextColorNav));
                 setToolbarMenu(R.menu.toolbar_schedule);
                 return true;
             } else if (itemId == R.id.nav_setting) {
                 changeFragment(new SettingFragment());
-                getSupportActionBar().setTitle(getColoredSpanned("나의 메뉴", customTextColor));
+                getSupportActionBar().setTitle(getColoredSpanned("나의 메뉴", customTextColorNav));
                 setToolbarMenu(R.menu.toolbar_setting);
                 return true;
             }
 
             return false;
         });
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //아무것도 선택하지 않았을 경우 홈을 누른것처럼 툴바의 메뉴가 보이게 설정!
         int selectedItem = binding.bottomNav.getSelectedItemId();
-
         if (selectedItem == R.id.nav_home) {
             getMenuInflater().inflate(R.menu.toolbar_home, menu);
-
-        } else {
-            // getMenuInflater().inflate(R.menu.toolbar_home, menu);
         }
 
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){//툴바의 메뉴생성
-            getMenuInflater().inflate(R.menu.toolbar_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if  (id == R.id.menu_logout) {
+            SharedPreferences pref = getSharedPreferences("PROJECT_MEMBER", MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            Intent intent = new Intent(this, LoginBeforeActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        } else if (id == R.id.menu_alam) {
+            // 처리 추가
+            return true;
+        } else if (id == android.R.id.home) {
+            finish();
             return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
 
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){//툴바선택시....
-            int id = item.getItemId();
+    private SpannableString getColoredSpanned(String text, int color) {
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new ForegroundColorSpan(color), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannableString;
+    }
 
-            if (id == R.id.menu_menu) {
+    public void changeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
 
+    private void showEmergencyDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_emergency, null);
+        builder.setView(dialogView);
+
+        TextView dialogTitle = dialogView.findViewById(R.id.dialogTitle);
+        TextView dialogMessage = dialogView.findViewById(R.id.dialogMessage);
+        Button confirmButton = dialogView.findViewById(R.id.confirmButton);
+        Button cancelButton = dialogView.findViewById(R.id.cancelButton);
+
+        dialogTitle.setText("긴급 전화");
+        dialogMessage.setText("긴급 전화 연결됩니다.\n 연결하시겠습니까?");
+
+        confirmButton.setOnClickListener(v -> {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:긴급전화번호"));
+            startActivity(callIntent);
+
+            alertDialog.dismiss();
+        });
+
+        cancelButton.setOnClickListener(v -> alertDialog.dismiss());
+
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void setToolbarMenu(int menuResId) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.getMenu().clear();
+        getMenuInflater().inflate(menuResId, toolbar.getMenu());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "한번 더 뒤로가기를 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
+        }
+    }
+
+    //포그라운드 서비스 작동중 체크
+    public boolean isForegroundServiceRunning(Context context, int notificationId) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        StatusBarNotification[] notifications = manager.getActiveNotifications();
+        for (StatusBarNotification notification : notifications) {
+            if (notification.getId() == notificationId) {
                 return true;
-            } else if (id == R.id.menu_logout) {
-                SharedPreferences pref = getSharedPreferences("PROJECT_MEMBER", MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.clear();
-                if (id == R.id.menu_logout) {
-                    Intent intent = new Intent(this, LoginBeforeActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return true;
-                } else if (id == R.id.menu_setting) {
-
-                    return true;
-                } else if (id == android.R.id.home) { //toolbar의 back키 눌렀을 때 동작
-                    finish();
-                    return true;
-                }
-                // 다른 메뉴 아이템에 대한 처리 추가 가능
-
-                return super.onOptionsItemSelected(item);
-            }
-
-            private SpannableString getColoredSpanned (String text,int color){//툴바의 텍스트색상설정
-                SpannableString spannableString = new SpannableString(text);
-                spannableString.setSpan(new ForegroundColorSpan(color), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                return spannableString;
-            }
-
-            public void changeFragment (Fragment fragment){
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
-
-            private void showEmergencyDialog () {//긴급전화 다이얼로그
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                View dialogView = getLayoutInflater().inflate(R.layout.dialog_emergency, null);
-                builder.setView(dialogView);
-
-                TextView dialogTitle = dialogView.findViewById(R.id.dialogTitle);
-                TextView dialogMessage = dialogView.findViewById(R.id.dialogMessage);
-                Button confirmButton = dialogView.findViewById(R.id.confirmButton);
-                Button cancelButton = dialogView.findViewById(R.id.cancelButton);
-
-                dialogTitle.setText("긴급 전화");
-                dialogMessage.setText("긴급 전화 연결됩니다.\n 연결하시겠습니까?");
-
-                confirmButton.setOnClickListener((new View.OnClickListener() {//긴급전화 확인버튼
-                    @Override
-                    public void onClick(View v) {
-                        Intent callIntent = new Intent(Intent.ACTION_CALL);
-                        callIntent.setData(Uri.parse("tel:긴급전화번호"));
-                        startActivity(callIntent);
-
-                        alertDialog.dismiss();
-                    }
-                }));
-
-                cancelButton.setOnClickListener(new View.OnClickListener() {//긴급전화 취소버튼
-                    @Override
-                    public void onClick(View v) {
-                        alertDialog.dismiss();
-                    }
-                });
-                alertDialog = builder.create();
-
-                alertDialog.show();
-            }
-
-            public void changeScheduleFragment (ScheduleFragment fragment){//일정관리메뉴 화면
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();//
-            }
-
-            private void setToolbarMenu ( int menuResId){//툴바메뉴 생성
-                Toolbar toolbar = findViewById(R.id.toolbar);
-                toolbar.getMenu().clear();
-                getMenuInflater().inflate(menuResId, toolbar.getMenu());
-            }
-
-            private void updateNotificationCount ( int count){//새로운 알림이 뜨면 업데이트된 알림갯수가 표시됨
-                MenuItem menuItem = binding.toolbar.getMenu().findItem(R.id.menu_alam_notification_count);
-
-                if (menuItem != null) {
-                    menuItem.setTitle(String.valueOf(count));
-                    menuItem.setVisible(count > 0);
-                }
-            }
-
-
-            @Override
-            public void onBackPressed () {
-                if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
-                    if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
-                        getSupportFragmentManager().popBackStack();// Fragment를 뒤로가기
-
-                    } else {
-                        if (doubleBackToExitPressedOnce) {
-                            super.onBackPressed();
-
-                        }
-
-                        this.doubleBackToExitPressedOnce = true;
-                        Toast.makeText(this, "한번 더 뒤로가기를 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show();
-
-                        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000); // 2초 동안 한번 더 눌러야 종료
-                    }
-
-
-                }
-
             }
         }
+        return false;
+    }
 
+}
