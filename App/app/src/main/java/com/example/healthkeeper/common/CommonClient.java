2165b0b9838479cd6923f2c9e6fd9521bfc13642
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2024-03-25 12:05:35 +0900

    aaa

--- App/app/src/main/java/com/example/healthkeeper/common/CommonClient.java
+++ App/app/src/main/java/com/example/healthkeeper/common/CommonClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class CommonClient {
     public static Retrofit getRetrofit(){
 
-        //Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.49/app/")
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.46/main/")
                 .addConverterFactory(ScalarsConverterFactory.create()).build();
         return retrofit;
     }
